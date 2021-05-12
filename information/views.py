b85a53bf889f2e497777a1c5d83d520e5a235e60
Siddhi Pevekar <pevekarsiddhi@gmail.com>
2021-05-12 23:55:50 +0530

    matplotlib

--- information/views.py
+++ information/views.py
@@ -4,7 +4,9 @@ from . import models
 from django.shortcuts import render
 import requests
 from .models import Supplier
+from matplotlib import pyplot as plt
 
+loggedin =""
 
 def download_data() -> dict:
     """
@@ -33,16 +35,54 @@ def homepage(request):
             'deaths' : country_data["deaths"], 
             'state_list' : state_data                 # List of states(table-rows)
         }
-
+    create_image(state_data)
+    
     return render(request, 'information/index.html' , d)
+
+def create_image(state_data):
+    data = []
+    # print(state_list)
+    for state in state_data:
+        cur = [ state['cases'] ,state['recoveries'] ,state['deaths'] , state['state'] ]
+
+        data.append(cur)
+
+    data.sort(reverse = True)
+    data = data[:10]
+
+    print(len(data))
+
+    y = [d[0] for d in data]
+    x = [d[-1] for d in data]
+
+    # print(x, y)
+    plt.figure(figsize=(15, 5))
+    print(x)
+    plt.bar(x,y,color='#2222AF')
+    plt.xlabel('States')
+    plt.ylabel('Active Cases')
+
+    # plt.show()
+    # pprint(data)
+    plt.savefig('information/static/information/assets/img/s1.png')
+
+if __name__ == '__main__':
+    data_dict = download_data()
+    country_data = data_dict['totals']
+    state_list = data_dict['states']
+    create_image(state_list)
 
 
 def s_loginpage(request):
+    global loggedin
     if request.method == "POST":
         username = request.POST.get('s_emailid', False)
         password= request.POST.get('s_password', False)
         print(username)
         print(password)
+
+        loggedin = username
+        print(loggedin)
 
         bool_ans = models.Supplier.objects.filter(s_emailid=username, s_password=password).exists()#condition to match values are equal or not
 
@@ -50,24 +90,19 @@ def s_loginpage(request):
             sup= models.Supplier.objects.filter(s_emailid = username)
             supval = {
                 "supplier": sup #to get particular object
-            }
-            
+            }           
 
-            return render(request, 'information/supplier_home.html',supval)
-            
+            return render(request, 'information/supplier_home.html',supval)            
 
         if bool_ans == False:
 
             return render(request, 'information/index.html')
-        
-        # if request.method == "POST":
-        #     s_agency_name= request.POST.get('s_agency_name', False)
-        #     supplier_record = models.Supplier(s_agency_name=s_agency_name)
-        #     supplier_record.save()
-        # return render(request, 'information/supplier_home.html')
-
 
     return render(request, 'information/supplier_home.html')#where the form is present
+
+# def update_supplier(request):
+
+    
 
 
 def patient_register(request):
@@ -112,6 +147,4 @@ def patient_login(request):
     return render(request, 'information/patient_homepage.html', {"supplier_info": data})
 
 
-# def edit_supplier_profile(request):
-#     data = Supplier.objects.get()
-#     return render(request, "information/edit_supplierprofile.html")
+
