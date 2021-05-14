e910fe9b2484faeeb9e08922346df377b337c375
Siddhi Pevekar <pevekarsiddhi@gmail.com>
2021-05-14 23:52:24 +0530

    updating

--- information/views.py
+++ information/views.py
@@ -6,7 +6,8 @@ import requests
 from .models import Supplier
 from matplotlib import pyplot as plt
 
-loggedin =""
+user_s =""
+user_p =""
 
 def download_data() -> dict:
     """
@@ -74,15 +75,15 @@ if __name__ == '__main__':
 
 
 def s_loginpage(request):
-    global loggedin
+    global user_s
     if request.method == "POST":
         username = request.POST.get('s_emailid', False)
         password= request.POST.get('s_password', False)
         print(username)
         print(password)
 
-        loggedin = username
-        print(loggedin)
+        user_s = username
+        print(user_s)
 
         bool_ans = models.Supplier.objects.filter(s_emailid=username, s_password=password).exists()#condition to match values are equal or not
 
@@ -114,14 +115,14 @@ def patient_register(request):
    return render(request, 'information/index.html')
 
 def patient_login(request):
-    # global user_g
+    global user_p
     data = Supplier.objects.all()
     print(data)
     if request.method == "POST":
         p_username = request.POST.get('p_username', False)
         p_pass1= request.POST.get('p_pass1', False)
         
-        # user_g = username
+        user_p = p_username
         print(p_username)
         print(p_pass1)
 
@@ -139,35 +140,49 @@ def patient_login(request):
         if bool_ans == False:
 
             return render(request, 'information/index.html')
-    return render(request, 'information/patient_homepage.html', {"supplier_info": data})
+    return render(request, 'information/patient_homepage.html')
 
 
 
 def update_supplier(request):
-    if request.method == 'POST':
-        s_id = request.POST.get('s_id')
-        s_agency_name = request.POST.get('s_agency_name')
-        s_state = request.POST.get('s_state')
-        s_emailid = request.POST.get('s_emailid')
-        s_password = request.POST.get('s_password')
-        s_district = request.POST.get('s_district')
-        icu_beds = request.POST.get('icu_beds')
-        ventilator_beds= request.POST.get('ventilator_beds')
-        icu_ventilator_beds = request.POST.get('icu_ventilator_beds')
-        oxygen = request.POST.get('oxygen')
-        bool_ans = models.Supplier.objects.filter(s_id=s_id).exists()#condition to match values are equal or not
-
-        if bool_ans == True:
-            supplier_record = models.Supplier( s_id=s_id,s_agency_name=s_agency_name, s_state=s_state, s_emailid=s_emailid, s_password= s_password, s_district=s_district, icu_beds = icu_beds, ventilator_beds=ventilator_beds, icu_ventilator_beds=icu_ventilator_beds, oxygen=oxygen)
-            supplier_record.save()
-            print("Data has been saved")
-            # Supplier.objects.filter(pk=request.POST.get('s_id')).update(
-            #     s_agency_name=s_agency_name,
-            #     s_state=s_state
-            # )
-
-            return render(request, 'information/index.html')
-    return render(request, 'information/index.html')
+    global user_s 
+    if user_s!="":
+        print(user_s)
+        #supp is kind array of object
+        supp = models.Supplier.objects.filter(s_emailid=user_s)
+        if request.method == 'POST':
+            # s_id = request.POST.get('s_id')
+            s_agency_name = request.POST.get('s_agency_name')
+            s_state = request.POST.get('s_state')
+            s_emailid = request.POST.get('s_emailid')
+            s_password = request.POST.get('s_password')
+            s_district = request.POST.get('s_district')
+            icu_beds = request.POST.get('icu_beds')
+            ventilator_beds= request.POST.get('ventilator_beds')
+            icu_ventilator_beds = request.POST.get('icu_ventilator_beds')
+            oxygen = request.POST.get('oxygen')
+            for i in supp:
+                i.s_agency_name = s_agency_name
+                i.save()
+                i.s_state = s_state
+                i.save()
+                i.s_emailid = s_emailid
+                i.save()
+                i.s_password = s_password
+                i.save()
+                i.s_district = s_district
+                i.save()
+                i.icu_beds = icu_beds
+                i.save()
+                i.ventilator_beds = ventilator_beds
+                i.save()
+                i.icu_ventilator_beds = icu_ventilator_beds
+                i.save()
+                i.oxygen = oxygen
+                i.save() 
+            print("Details saved")
+            supplier = {
+                "sup": supp
+            }
+    return render(request, 'information/supplier_home.html', supplier)
            
-
-
